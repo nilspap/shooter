d7f9c2602204ccf773bd52a5d5cbc289f5412712
nilspap <157360282+nilspap@users.noreply.github.com>
2024-01-25 15:25:55 +0100

    gamestate

--- server.js
+++ server.js
@@ -10,12 +10,35 @@ const server = http.createServer(app);
 server.listen(8080, function () {
     console.log('Listening on http://localhost:8080');
 });
+const gameState = {
+    players: [{
+        id: "player1",
+        x: 0,
+        y: 0
+    }]
+}
 wss.on('connection', function connection(ws) {
     ws.on('error', console.error);
 
     ws.on('message', function message(data) {
         console.log('received: %s', data);
+        const command = JSON.parse(data);
+        const player = gameState.players.find(player => player.id == command.player);
+        if (command.action == "down") {
+            player.y += 1;
+        }
+        if (command.action == "up") {
+            player.y -= 1;
+        }
+        if (command.action == "left") {
+            player.x -= 1;
+        }
+        if (command.action == "right") {
+            player.x += 1;
+        }
+        console.log(JSON.stringify(gameState))
+        ws.send(JSON.stringify(gameState));
     });
 
-    ws.send('something');
+    //ws.send('something');
 });
