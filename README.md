390542a8aea50f6ed1c7faf8f3db7d4f9d8485e9
nilspap <157360282+nilspap@users.noreply.github.com>
2024-01-26 12:24:10 +0100

    unterschiedliche player

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# shooter
-shooter
+# Shooter
+
+## Instalation
+
+- NodeJS installieren: https://nodejs.org/en/download/
+- Terminal `npm install` hiermit holt sich der npm(node package manager) die Abhängigkeiten
+- Terminal `npm start`
+- dann mit strg die url im terminal anklicken 
--- server.js
+++ server.js
@@ -11,12 +11,10 @@ server.listen(8080, function () {
     console.log('Listening on http://localhost:8080');
 });
 const gameState = {
-    players: [{
-        id: "player1",
-        x: 0,
-        y: 0
-    }]
+    players: []
 }
+const playerSpeed = 5;
+let playerCounter = 1;
 function distributeState() {
     wss.clients.forEach(function each(client) {
         if (client.readyState === WebSocket.OPEN) {
@@ -25,24 +23,32 @@ function distributeState() {
     });
 }
 wss.on('connection', function connection(ws) {
+    const player = {
+        id: `player${playerCounter}`,
+        x: 0,
+        y: 0
+    };
+    playerCounter += 1;
+    gameState.players.push(player);
     distributeState();
+
     ws.on('error', console.error);
 
     ws.on('message', function message(data) {
         console.log('received: %s', data);
         const command = JSON.parse(data);
-        const player = gameState.players.find(player => player.id == command.player);
         if (command.action == "down") {
-            player.y += 1;
+            player.y += playerSpeed;
         }
         if (command.action == "up") {
-            player.y -= 1;
+            player.y -= playerSpeed;
         }
         if (command.action == "left") {
-            player.x -= 1;
+            player.x -= playerSpeed;
+            ;
         }
         if (command.action == "right") {
-            player.x += 1;
+            player.x += playerSpeed;
         }
         console.log(JSON.stringify(gameState))
         distributeState();
