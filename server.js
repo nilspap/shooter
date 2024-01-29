216ce063418ad3b88e702b427ae999a744bd47ba
nilspap <157360282+nilspap@users.noreply.github.com>
2024-01-29 11:14:39 +0100

    victim

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ const gameState = {
 const playerSpeed = 5;
 const bulletSpeed = 10;
 const flightDistance = 100;
+const frameDuration = 16;
+const playerSize = 10;
+const bulletSize = 2;
 let playerCounter = 1;
 function distributeMessage(message) {
     const messageString = JSON.stringify(message);
@@ -35,59 +38,106 @@ wss.on('connection', function connection(ws) {
         type: "currentPlayerId",
         playerId: playerId
     }));
-    const player = {
+    const currentPlayer = {
         id: playerId,
         x: 0,
         y: 0,
         aimDirection: "right"
     };
     playerCounter += 1;
-    gameState.players.push(player);
+    gameState.players.push(currentPlayer);
     distributeState();
 
     ws.on('error', console.error);
     function movePlayer(moveDirection) {
         if (moveDirection == "down") {
-            player.y += playerSpeed;
+            currentPlayer.y += playerSpeed;
 
         }
         if (moveDirection == "up") {
-            player.y -= playerSpeed;
+            currentPlayer.y -= playerSpeed;
 
         }
         if (moveDirection == "left") {
-            player.x -= playerSpeed;
+            currentPlayer.x -= playerSpeed;
 
         }
         if (moveDirection == "right") {
-            player.x += playerSpeed;
+            currentPlayer.x += playerSpeed;
 
         }
-        player.aimDirection = moveDirection;
+        currentPlayer.aimDirection = moveDirection;
         distributeState();
     }
+    function moveBullet(message) {
+        let flightEnded = false;
+        if (message.flightDirection == "up") {
+            message.y -= message.bulletSpeed;
+            if (message.y <= message.bulletTargetY) {
+                flightEnded = true;
+            }
+        } else if (message.flightDirection == "down") {
+            message.y += message.bulletSpeed;
+            if (message.y >= message.bulletTargetY) {
+                flightEnded = true;
+            }
+        } else if (message.flightDirection == "left") {
+            message.x -= message.bulletSpeed;
+            if (message.x <= message.bulletTargetX) {
+                flightEnded = true;
+            }
+        } else if (message.flightDirection == "right") {
+            message.x += message.bulletSpeed;
+            if (message.x >= message.bulletTargetX) {
+                flightEnded = true;
+            }
+        }
+        hitCalculation(message);
+        if (flightEnded == false) {
+            setTimeout(() => moveBullet(message), message.frameDuration);
+        }
+    }
+    function hitCalculation(bullet) {
+        for (const player of gameState.players) {
+            if (player == currentPlayer) {
+                continue;
+            }
+            if (bullet.x >= player.x
+                && (bullet.x - bulletSize) <= (player.x + playerSize)
+                && bullet.y >= player.y
+                && (bullet.y - bulletSize) <= player.y + playerSize) {
+                distributeMessage({
+                    type: "hit",
+                    victim: player.id
+                })
+            }
+        }
+    }
     function shoot() {
-        let bulletTargetX = player.x;
-        let bulletTargetY = player.y;
-        if (player.aimDirection == "up") {
+        let bulletTargetX = currentPlayer.x;
+        let bulletTargetY = currentPlayer.y;
+        if (currentPlayer.aimDirection == "up") {
             bulletTargetY -= flightDistance;
-        } else if (player.aimDirection == "down") {
+        } else if (currentPlayer.aimDirection == "down") {
             bulletTargetY += flightDistance;
-        } else if (player.aimDirection == "left") {
+        } else if (currentPlayer.aimDirection == "left") {
             bulletTargetX -= flightDistance;
-        } else if (player.aimDirection == "right") {
+        } else if (currentPlayer.aimDirection == "right") {
             bulletTargetX += flightDistance;
         }
-        distributeMessage({
+        const message = {
             type: "bullet",
-            x: player.x,
-            y: player.y,
-            flightDirection: player.aimDirection,
+            x: currentPlayer.x,
+            y: currentPlayer.y,
+            flightDirection: currentPlayer.aimDirection,
             bulletSpeed: bulletSpeed,
             shooterId: playerId,
             bulletTargetX: bulletTargetX,
-            bulletTargetY: bulletTargetY
-        });
+            bulletTargetY: bulletTargetY,
+            frameDuration: frameDuration
+        };
+        moveBullet(message);
+        distributeMessage(message);
     }
     ws.on('message', function message(data) {
         console.log('received: %s', data);
